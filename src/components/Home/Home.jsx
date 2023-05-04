ea9708c426ba041c0d228bdfc19ddbe9159854e5
Sagar Jana <64752565+sagarjana531@users.noreply.github.com>
2023-05-05 01:09:26 +0530

    home start

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import Home_Carousal from './Home_Carousal';
 const Home = () => {
   return (
     <div>
       <h1>This is Home</h1>
+      <Home_Carousal/>
+
     </div>
   )
 }
 
-export default Home
+export default Home;
