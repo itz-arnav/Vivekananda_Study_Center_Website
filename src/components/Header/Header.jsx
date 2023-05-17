6f5865f9c3eccdd6b2b5b67edd60d3c421e271e9
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2023-05-17 20:39:07 +0530

    finished footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.png";
+import { AiOutlineDown, AiOutlineRight } from 'react-icons/ai';
+
 const Header = () => {
 	
 
@@ -9,17 +11,21 @@ const Header = () => {
       <div className="headerContent">
       <div className="leftSection">
       <div className="imageSection">
-        <img src={Logo} alt="" srcset="" className="imageIcon"/>
+        <img src={Logo} alt="" className="imageIcon"/>
       </div>
       <ul className="nav-links">
         <li>
           <a href="/">Home</a>
         </li>
         <li>
-          <a href="/" className="dropdown">Courses v</a>
+          <a href="/" className="dropdown">Courses <AiOutlineDown className="iconsize"/></a>
           <ul className="primary-dropdown">
             <li>
-              <a href="/">Boards &gt;</a>
+              <div className="subsection">
+                <a href="/">Boards</a>
+                <AiOutlineRight className="iconsize"/>
+              </div>
+              
               <ul className="secondary-dropdown">
                 <li>
                   <a href="/">CBSE</a>
@@ -33,7 +39,10 @@ const Header = () => {
               </ul>
             </li>
             <li>
-              <a href="/">Competitive Exams &gt;</a>
+            <div className="subsection">
+                <a href="/">Competitive Exams</a>
+                <AiOutlineRight className="iconsize"/>
+              </div>
               <ul className="secondary-dropdown">
                 <li>
                   <a href="/">WBJEE</a>
@@ -57,8 +66,8 @@ const Header = () => {
       </ul>
       </div>
       <div className="signupSection">
-      <button class="log">Login</button>
-    <button class="reg">Sign up</button>
+      <button className="log">Login</button>
+    <button className="reg">Sign up</button>
       </div>
       </div>
     </nav>
