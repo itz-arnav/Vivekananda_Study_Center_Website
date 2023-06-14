d6343e8a86835a165d2d935c817c0535bea8a30c
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2023-06-15 00:49:15 +0530

    added section

--- src/components/Home/Preparing/Preparing.jsx
+++ src/components/Home/Preparing/Preparing.jsx
@@ -1,29 +1,296 @@
 import "./Preparing.css";
+import courseImage from "../../../assets/5353134.jpg";
+import { BiRupee } from 'react-icons/bi';
+import { AiFillStar, AiFillClockCircle } from 'react-icons/ai';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 const Preparing = () => {
-  return (
-    <div className="preparingContainer">
-      <h2>I am Preparing For</h2>
-      <p>We have a variety of courses to choose from</p>
-      <div className="courseContainer">
-        <input type="radio" name="slider" id="cbse" Checked />
-        <input type="radio" name="slider" id="icse"  />
-        <input type="radio" name="slider" id="wbse"  />
-        <input type="radio" name="slider" id="jee_neet"  />
-        <input type="radio" name="slider" id="kvpy"  />
+	return (
+		<div className="preparingContainer">
+			<h2>I am Preparing For</h2>
+			<p>We have a variety of courses to choose from</p>
+			<div className="courseContainer">
+				<input type="radio" name="slider" id="cbse" defaultChecked />
+				<input type="radio" name="slider" id="icse" />
+				<input type="radio" name="slider" id="wbse" />
+				<input type="radio" name="slider" id="jee_neet" />
+				<input type="radio" name="slider" id="kvpy" />
 
-        <nav>
-            <label htmlFor="cbse" className="cbse">CBSE</label>
-            <label htmlFor="icse" className="icse">ICSE/ISC</label>
-            <label htmlFor="wbse" className="wbse">WBSSE/WBCHSE</label>
-            <label htmlFor="jee_neet" className="jee_neet">JEE/NEET</label>
-            <label htmlFor="kvpy" className="kvpy">KVPY</label>
-        </nav>
+				<nav>
+					<label htmlFor="cbse" className="cbse">
+						CBSE
+					</label>
+					<label htmlFor="icse" className="icse">
+						ICSE/ISC
+					</label>
+					<label htmlFor="wbse" className="wbse">
+						WBSSE/WBCHSE
+					</label>
+					<label htmlFor="jee_neet" className="jee_neet">
+						JEE/NEET
+					</label>
+					<label htmlFor="kvpy" className="kvpy">
+						KVPY
+					</label>
+				</nav>
 
-        <section></section>
-      </div>
-    </div>
-  )
-}
+				<section>
+					<div className="courseContent cbse">
+						<img src={courseImage} alt="courseImage" />
+						<div className="dataSection">
+							<h2>Complete Course for Class VI</h2>
+							<div className="detailSection">
+                                <div className="ratingSection">
+                                    <p className="ratingValue">4.9</p>
+                                    <div className="stars">
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                    </div>
+                                </div>
+                                <div className="durationSection">
+                                    <AiFillClockCircle />
+                                    <p className="durationValue">73.2h</p>
+                                </div>
+                            </div>
+							<hr />
+							<div className="priceSection">
+								<div className="priceValue">
+									<div className="currentPrice">
+										<BiRupee className="priceIcon" />
+										<div className="currentPriceValue">499</div>
+									</div>
+                                    <div className="oldPrice">
+                                    <BiRupee className="priceIcon" />
+										<div className="oldPriceValue">799</div>
+                                    </div>
+								</div>
+                                <Link to="/"><button className="buyNowButton">Buy Now</button></Link>
+							</div>
+						</div>
+					</div>
+					<div className="courseContent cbse">
+						<img src={courseImage} alt="courseImage" />
+						<div className="dataSection">
+							<h2>Complete Course for Class VII</h2>
+							<div className="detailSection">
+                                <div className="ratingSection">
+                                    <p className="ratingValue">4.9</p>
+                                    <div className="stars">
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                    </div>
+                                </div>
+                                <div className="durationSection">
+                                    <AiFillClockCircle />
+                                    <p className="durationValue">73.2h</p>
+                                </div>
+                            </div>
+							<hr />
+							<div className="priceSection">
+								<div className="priceValue">
+									<div className="currentPrice">
+										<BiRupee className="priceIcon" />
+										<div className="currentPriceValue">499</div>
+									</div>
+                                    <div className="oldPrice">
+                                    <BiRupee className="priceIcon" />
+										<div className="oldPriceValue">799</div>
+                                    </div>
+								</div>
+                                <Link to="/"><button className="buyNowButton">Buy Now</button></Link>
+							</div>
+						</div>
+					</div>
+					<div className="courseContent cbse">
+						<img src={courseImage} alt="courseImage" />
+						<div className="dataSection">
+							<h2>Complete Course for Class VIII</h2>
+							<div className="detailSection">
+                                <div className="ratingSection">
+                                    <p className="ratingValue">4.9</p>
+                                    <div className="stars">
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                    </div>
+                                </div>
+                                <div className="durationSection">
+                                    <AiFillClockCircle />
+                                    <p className="durationValue">73.2h</p>
+                                </div>
+                            </div>
+							<hr />
+							<div className="priceSection">
+								<div className="priceValue">
+									<div className="currentPrice">
+										<BiRupee className="priceIcon" />
+										<div className="currentPriceValue">499</div>
+									</div>
+                                    <div className="oldPrice">
+                                    <BiRupee className="priceIcon" />
+										<div className="oldPriceValue">799</div>
+                                    </div>
+								</div>
+                                <Link to="/"><button className="buyNowButton">Buy Now</button></Link>
+							</div>
+						</div>
+					</div>
+					<div className="courseContent cbse">
+						<img src={courseImage} alt="courseImage" />
+						<div className="dataSection">
+							<h2>Complete Course for Class IX</h2>
+							<div className="detailSection">
+                                <div className="ratingSection">
+                                    <p className="ratingValue">4.9</p>
+                                    <div className="stars">
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                    </div>
+                                </div>
+                                <div className="durationSection">
+                                    <AiFillClockCircle />
+                                    <p className="durationValue">73.2h</p>
+                                </div>
+                            </div>
+							<hr />
+							<div className="priceSection">
+								<div className="priceValue">
+									<div className="currentPrice">
+										<BiRupee className="priceIcon" />
+										<div className="currentPriceValue">499</div>
+									</div>
+                                    <div className="oldPrice">
+                                    <BiRupee className="priceIcon" />
+										<div className="oldPriceValue">799</div>
+                                    </div>
+								</div>
+                                <Link to="/"><button className="buyNowButton">Buy Now</button></Link>
+							</div>
+						</div>
+					</div>
+					<div className="courseContent cbse">
+						<img src={courseImage} alt="courseImage" />
+						<div className="dataSection">
+							<h2>Complete Course for Class X</h2>
+							<div className="detailSection">
+                                <div className="ratingSection">
+                                    <p className="ratingValue">4.9</p>
+                                    <div className="stars">
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                    </div>
+                                </div>
+                                <div className="durationSection">
+                                    <AiFillClockCircle />
+                                    <p className="durationValue">73.2h</p>
+                                </div>
+                            </div>
+							<hr />
+							<div className="priceSection">
+								<div className="priceValue">
+									<div className="currentPrice">
+										<BiRupee className="priceIcon" />
+										<div className="currentPriceValue">499</div>
+									</div>
+                                    <div className="oldPrice">
+                                    <BiRupee className="priceIcon" />
+										<div className="oldPriceValue">799</div>
+                                    </div>
+								</div>
+                                <Link to="/"><button className="buyNowButton">Buy Now</button></Link>
+							</div>
+						</div>
+					</div>
+					<div className="courseContent cbse">
+						<img src={courseImage} alt="courseImage" />
+						<div className="dataSection">
+							<h2>Complete Course for Class XI</h2>
+							<div className="detailSection">
+                                <div className="ratingSection">
+                                    <p className="ratingValue">4.9</p>
+                                    <div className="stars">
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                    </div>
+                                </div>
+                                <div className="durationSection">
+                                    <AiFillClockCircle />
+                                    <p className="durationValue">73.2h</p>
+                                </div>
+                            </div>
+							<hr />
+							<div className="priceSection">
+								<div className="priceValue">
+									<div className="currentPrice">
+										<BiRupee className="priceIcon" />
+										<div className="currentPriceValue">499</div>
+									</div>
+                                    <div className="oldPrice">
+                                    <BiRupee className="priceIcon" />
+										<div className="oldPriceValue">799</div>
+                                    </div>
+								</div>
+                                <Link to="/"><button className="buyNowButton">Buy Now</button></Link>
+							</div>
+						</div>
+					</div>
+					<div className="courseContent cbse">
+						<img src={courseImage} alt="courseImage" />
+						<div className="dataSection">
+							<h2>Complete Course for Class XII</h2>
+							<div className="detailSection">
+                                <div className="ratingSection">
+                                    <p className="ratingValue">4.9</p>
+                                    <div className="stars">
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                        <AiFillStar />
+                                    </div>
+                                </div>
+                                <div className="durationSection">
+                                    <AiFillClockCircle />
+                                    <p className="durationValue">73.2h</p>
+                                </div>
+                            </div>
+							<hr />
+							<div className="priceSection">
+								<div className="priceValue">
+									<div className="currentPrice">
+										<BiRupee className="priceIcon" />
+										<div className="currentPriceValue">499</div>
+									</div>
+                                    <div className="oldPrice">
+                                    <BiRupee className="priceIcon" />
+										<div className="oldPriceValue">799</div>
+                                    </div>
+								</div>
+                                <Link to="/"><button className="buyNowButton">Buy Now</button></Link>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		</div>
+	);
+};
 
-export default Preparing
+export default Preparing;
