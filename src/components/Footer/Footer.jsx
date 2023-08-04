1b48c49885e785b97bb4792398bc72fa4494a276
Sagar Jana <64752565+sagarjana531@users.noreply.github.com>
2023-08-04 14:13:48 +0530

    change footer.css to module.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,82 +12,85 @@ import { MdLocationOn, MdPhoneCallback, MdEmail } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 
-import "./Footer.css";
+import css from"../Footer/Footer.module.css";
 
 const Footer = () => {
   return (
-    <div className='footerContainer'>
-      <div className="footerContent">
-        <div className="linkSection">
-          <div className="companyDetails">
-            <div className="companyDetailsContainer titleContainer">
-            <HiAcademicCap className='companyDetailsNameIcon titleIcon'/>
-            <h2 className="companyDetailsName companytitleName">Vivekananda Study Center</h2>
+    <>
+    <div className={css.footerContainer}>
+      <div className={css.footerContent}>
+        <div className={css.linkSection}>
+          <div className={css.companyDetails}>
+            <div className={`${css.companyDetailsContainer} ${css.titleContainer}`}>
+            <HiAcademicCap className={`${css.companyDetailsContainer} ${css.titleContainer}`}/>
+            <h2 className={`${css.companyDetailsContainer} ${css.companytitleName}`}>Vivekananda Study Center</h2>
             </div>
-            <div className="companyDetailsContainer">
-            <MdLocationOn className='companyDetailsNameIcon'/>
-            <h3 className="companyDetailsName">CIT Road, Salt Lake Sector 4, Kolkata - 700098</h3>
+            <div className={css.companyDetailsContainer}>
+            <MdLocationOn className={css.companyDetailsNameIcon}/>
+            <h3 className={css.companyDetailsName}>CIT Road, Salt Lake Sector 4, Kolkata - 700098</h3>
             </div>
-            <div className="companyDetailsContainer">
-            <MdPhoneCallback className='companyDetailsNameIcon'/>
-            <h3 className="companyDetailsName">(033) 123-456-789</h3>
+            <div className={css.companyDetailsContainer}>
+            <MdPhoneCallback className={css.companyDetailsNameIcon}/>
+            <h3 className={css.companyDetailsName}>(033) 123-456-789</h3>
             </div>
-            <div className="companyDetailsContainer">
-            <MdEmail className='companyDetailsNameIcon'/>
-            <h3 className="companyDetailsName">test@gmail.com</h3>
+            <div className={css.companyDetailsContainer}>
+            <MdEmail className={css.companyDetailsNameIcon}/>
+            <h3 className={css.companyDetailsName}>test@gmail.com</h3>
             </div>
 
           </div>
-          <div className="AboutDetails">
-          <h2 className='titleName'>About</h2>
-          <Link className='footerLink' to="/courses">Courses</Link>
-          <Link className='footerLink' to="/mentors">Mentors</Link>
-          <Link className='footerLink' to="/refund">Refund</Link>
-          <Link className='footerLink' to="/conditions">Terms & Conditions</Link>
-          <Link className='footerLink' to="/payment">Payment</Link>
+          <div className={css.AboutDetails}>
+          <h2 className={css.titleName}>About</h2>
+          <Link className={css.footerLink} to="/courses">Courses</Link>
+          <Link className={css.footerLink} to="/mentors">Mentors</Link>
+          <Link className={css.footerLink} to="/refund">Refund</Link>
+          <Link className={css.footerLink} to="/conditions">Terms & Conditions</Link>
+          <Link className={css.footerLink} to="/payment">Payment</Link>
           </div>
-          <div className="CategoryDetails">
-          <h2 className='titleName'>Category</h2>
-          <Link className='footerLink' to="/courses">Boards</Link>
-          <Link className='footerLink' to="/mentors">JEE/NEET</Link>
-          <Link className='footerLink' to="/refund">Government Exams</Link>
-          <Link className='footerLink' to="/conditions">State Exams</Link>
-          <Link className='footerLink' to="/payment">Private Jobs</Link>
+          <div className={css.CategoryDetails}>
+          <h2 className={css.titleName}>Category</h2>
+          <Link className={css.footerLink} to="/courses">Boards</Link>
+          <Link className={css.footerLink} to="/mentors">JEE/NEET</Link>
+          <Link className={css.footerLink} to="/refund">Government Exams</Link>
+          <Link className={css.footerLink} to="/conditions">State Exams</Link>
+          <Link className={css.footerLink} to="/payment">Private Jobs</Link>
           </div>
-          <div className="getInTouch">
-          <h2 className='titleName'>Get in Touch</h2>
-          <h3 className='normalText'>Send us your email, we'll make sure you never miss a thing!</h3>
-          <div className="emailSubmitSection">
-            <div className="textSection">
-            <GrMailOption className='emailMailIcon'/>
-            <input type="text" className='emailInputText' placeholder='Your Email'/>
+          <div className={css.getInTouch}>
+          <h2 className={css.titleName}>Get in Touch</h2>
+          <h3 className={css.normalText}>Send us your email, we'll make sure you never miss a thing!</h3>
+          <div className={css.emailSubmitSection}>
+            <div className={css.textSection}>
+            <GrMailOption className={css.emailMailIcon}/>
+            <input type="text" className={css.emailInputText} placeholder="Your Email" />
             </div>
-            <button type="submit" className='emailbtn'>
-              <BsArrowRight className='emailIcon'/>
+            <button type="submit" className={css.emailbtn}>
+              <BsArrowRight className={css.emailIcon}/>
             </button>
           </div>
-          <div className="socialIconSection">
-            <RiFacebookCircleFill className='socialIconLogo'/>
-            <RiInstagramLine className='socialIconLogo'/>
-            <RiYoutubeFill className='socialIconLogo'/>
-            <RiTwitterFill className='socialIconLogo' />
+          <div className={css.socialIconSection}>
+            <RiFacebookCircleFill className={css.socialIconLogo}/>
+            <RiInstagramLine className={css.socialIconLogo}/>
+            <RiYoutubeFill className={css.socialIconLogo}/>
+            <RiTwitterFill className={css.socialIconLogo} />
           </div>
           </div>
         </div>
-        <hr className='footerHr'/>
-        <div className="copyrightSection">
-          <div className="copyrightText">
+        <hr className={css.footerHr}/>
+        <div className={css.copyrightSection}>
+          <div className={css.copyrightText}>
             Copyright @2023. All Rights Reserved.
           </div>
-          <div className="paymentSection">
+          <div className={css.paymentSection}>
             <img src={Apple} alt="" />
             <img src={Visa} alt="" />
             <img src={MasterCard} alt=""  />
             <img src={Paypal} alt="" />
           </div>
         </div>
-      </div>
+        </div>
     </div>
+     
+    </>
   )
 }
 
